e9853371d6a29cd9c329bd401c379cf12d2b570a
Anastasia <anastasia@sdikanskiy.local>
2023-04-24 01:16:06 +0300

    task2 check out sqr root

---
+++ task2/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("Введите подкоренное число ");
+string a = Console.ReadLine();
+
+Console.Write("Введите корень числа ");
+string b = Console.ReadLine();
+
+int c = Convert.ToInt32(a);
+int d = Convert.ToInt32(b);
+
+Console.Write("является ли корнем числа: "); 
+if(c == d * d) 
+{
+   Console.WriteLine("ДА");  
+}
+else Console.WriteLine("Нет");
+
