088be4ecb1da1c9b657fd910a93b861a5c33fd6b
Anastasia <anastasia@sdikanskiy.local>
2023-04-24 13:28:45 +0300

    task5 find max number of two

---
+++ homeTask1/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.WriteLine("Введите два числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+
+if(a > b){
+    Console.WriteLine("Максимальное число: ");
+    Console.WriteLine(a);  
+}
+else{
+    Console.WriteLine("Максимальное число: ");
+    Console.WriteLine(b);
+   
+}
