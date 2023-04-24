2b8ff792454176807094dc8b073d00b240609f2f
Anastasia <anastasia@sdikanskiy.local>
2023-04-24 14:47:10 +0300

    homeTask4 print even number

---
+++ homeTask4/Program.cs
@@ -0,0 +1,13 @@
+﻿        Console.WriteLine("Введите число ");
+        int N = Convert.ToInt32(Console.ReadLine());
+        int numb = 1;
+     
+        while(numb <= N ){
+            int modulo = numb % 2;
+            if(modulo == 0){
+                Console.WriteLine(numb);
+            }
+            numb++;
+            
+        }
+    
