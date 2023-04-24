a4cc8cd30928928fefc03e9bb477287c5f4be214
Anastasia <anastasia@sdikanskiy.local>
2023-04-24 13:45:25 +0300

    homeTask2 find the max number of three

---
+++ homeTask2/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Введите три числа числа ");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+int x = Convert.ToInt32(Console.ReadLine());
+
+int max = a;
+
+if (b > max) max = b;
+if (x > max) max = x;
+
+Console.Write("max = ");
+Console.WriteLine(max);
