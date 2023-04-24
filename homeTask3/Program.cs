60a160bad5a6a5e859133645c6d5278c6410e9c3
Anastasia <anastasia@sdikanskiy.local>
2023-04-24 13:58:30 +0300

    homeTask3 find the number with/or modulo

---
+++ homeTask3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int modulo = a % 2;
+if(modulo == 0){
+   Console.WriteLine("да"); 
+}
+else{
+    Console.WriteLine("нет"); 
+}
