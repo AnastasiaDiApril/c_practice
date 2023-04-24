42cf1fa99e18124a7ca5f8ca14203f089e334bdd
Anastasia <anastasia@sdikanskiy.local>
2023-04-24 13:12:43 +0300

    task5 last number

---
+++ task5/Program.cs
@@ -0,0 +1,9 @@
+﻿string s = Console.ReadLine();
+int a = Convert.ToInt32(s);
+if(a >= 100 && a <= 999){
+    int c = a % 10;
+    Console.WriteLine(c);
+}
+else{
+    Console.WriteLine("число не трехзначное");
+}    
