76caf48f4e43bce2604eda6817db27b6ba130d8d
Anastasia <anastasia@sdikanskiy.local>
2023-05-28 14:14:53 +0200

    check out weekend

---
+++ homeTask7/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 1:
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    Console.WriteLine("не выходной");
+    break;
+
+    case 6:
+    case 7:
+    Console.WriteLine("выходной");
+    break;
+
+    default:
+    Console.WriteLine("неверное значение");   
+    break; 
+}
