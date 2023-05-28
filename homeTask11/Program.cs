aa8342cde81aefa65bbd8d07151ddb4c33a6c869
Anastasia <anastasia@sdikanskiy.local>
2023-05-28 19:31:12 +0200

    degree numb

---
+++ homeTask11/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите степень ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+ int degreenumb = 1;
+for(int i = 1; i <= b; i++){
+    degreenumb = degreenumb * a;  
+}
+Console.WriteLine(degreenumb);
