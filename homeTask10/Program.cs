233b55f45dbac6f110dff7647eeb8839465717ee
Anastasia <anastasia@sdikanskiy.local>
2023-05-28 19:04:39 +0200

    palindrom

---
+++ homeTask10/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("Введите число ");
+string m = Console.ReadLine();
+int N = Convert.ToInt32(m);
+if (N > 9999 && N < 100000){
+    Console.Write("Палиндром: ");
+    if (m[0] == m[4] && m[1] == m[3]){
+        Console.WriteLine("да");
+    }
+    else Console.WriteLine("нет");
+}
+else Console.WriteLine("ошибка");
