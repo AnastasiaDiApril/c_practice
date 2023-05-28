ba4763e517b1df265dc3a24a107f3d744eed85bd
Anastasia <anastasia@sdikanskiy.local>
2023-05-28 13:15:50 +0200

    find second number from three

---
+++ homeTask5/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число ");
+int N = Convert.ToInt32(Console.ReadLine());
+if (N >= 100 && N <= 999){
+    int modulo = N % 100;
+    int numb = modulo / 10;
+    Console.WriteLine(numb);
+} 
+else Console.WriteLine("только трезначное число");
+     
