1244ac25b9074c906b9e802988345bfd85accae6
Anastasia <anastasia@sdikanskiy.local>
2023-05-28 14:02:59 +0200

    print 3thdigit of number

---
+++ homeTask6/Program.cs
@@ -0,0 +1,7 @@
+﻿Console.WriteLine("Введите число ");
+string m = Console.ReadLine();
+int N = Convert.ToInt32(m);
+if (N >= 100){
+    Console.WriteLine(m[2]);
+}
+else Console.WriteLine("третьей цифры нет");
