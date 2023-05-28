6a04247dc42952b374205f3b6fea3c3670b640a2
Anastasia <anastasia@sdikanskiy.local>
2023-05-28 18:24:40 +0200

    distance between 2 points

---
+++ task9/Program.cs
@@ -0,0 +1,9 @@
+﻿int x1 = Convert.ToInt32(Console.ReadLine());
+int y1 = Convert.ToInt32(Console.ReadLine());
+int x2 = Convert.ToInt32(Console.ReadLine());
+int y2 = Convert.ToInt32(Console.ReadLine());
+
+int a = x2 - x1;
+int b = y2 - y1;
+double distance = Math.Sqrt(a*a+b*b);
+Console.WriteLine("distance= " + distance);
