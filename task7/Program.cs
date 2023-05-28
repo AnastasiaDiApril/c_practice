7b6ec585a6166e9580a4fd99d059ecd8067ed2f4
Anastasia <anastasia@sdikanskiy.local>
2023-05-28 17:29:34 +0200

    show coordinations range

---
+++ task7/Program.cs
@@ -0,0 +1,20 @@
+﻿int a = Convert.ToInt32(Console.ReadLine());
+switch (a)
+{
+    case 1:
+    Console.WriteLine("x > 0, y > 0");
+    break;
+    case 2:
+    Console.WriteLine("x < 0, y > 0");
+    break;
+    case 3:
+    Console.WriteLine("x < 0, y < 0");
+    break;
+    case 4:
+    Console.WriteLine("x > 0, y < 0");
+    break;
+
+    default:
+    Console.WriteLine("неверное значение");   
+    break; 
+}
